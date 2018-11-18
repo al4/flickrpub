3a35f90ec2ea15402c151772c4281f3600512bfc
Alex Forbes <al4@users.noreply.github.com>
2018-11-18 15:29:06 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='flickrpub',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/al4/flickrpub',
+    license='MIT',
+    author='al4',
+    author_email='',
+    description=''
+)
